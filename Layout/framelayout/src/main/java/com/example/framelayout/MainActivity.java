de80eb1c15afe72d6b5a6419ab6916a787bd2373
ford_ji <ford_ji@epam.com>
2022-09-23 17:49:33 +0800

    add read photos demo

--- Layout/framelayout/src/main/java/com/example/framelayout/MainActivity.java
+++ Layout/framelayout/src/main/java/com/example/framelayout/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.framelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE2 = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int readGrant = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (readGrant != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_CALL_PHONE2);
+        } else {
+
+        }
     }
 }
